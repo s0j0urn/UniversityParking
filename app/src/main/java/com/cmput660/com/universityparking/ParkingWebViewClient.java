e961344823dfb4e9329993d0b23da2a2ba4e655d
s0j0urn <varunsapra.sep@gmail.com>
2016-03-25 15:29:57 -0600

    Icon Added.

--- app/src/main/java/com/cmput660/com/universityparking/ParkingWebViewClient.java
+++ app/src/main/java/com/cmput660/com/universityparking/ParkingWebViewClient.java
@@ -1,6 +1,5 @@
 package com.cmput660.com.universityparking;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.webkit.WebView;
@@ -9,7 +8,7 @@ import android.webkit.WebViewClient;
 
 public class ParkingWebViewClient extends WebViewClient {
 
-    ProgressDialog _dialog ;
+
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
         if (Uri.parse(url).getHost().endsWith("youtube.com")) {
